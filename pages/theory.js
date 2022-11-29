a91ae2fd29a71e4d6b655d5496af24636b980981
mansikeshattiwar <85787927+mansikeshattiwar@users.noreply.github.com>
2022-11-29 22:30:15 +0530

    Update theory.js

--- pages/theory.js
+++ pages/theory.js
@@ -7,26 +7,26 @@ function theory() {
     <div className={styles.main}>
         <h1 className={styles.title}>Theory</h1>
         <p className={styles.the_desc}>
-            Diffie-Hellman protocol allows two communicating parties, say Alice and Bob, to create a symmetric session key with out the need of a KDC (Key Distribution Center)
+            Diffie-Hellman protocol allows two communicating parties, say Alice and Bob, to create a symmetric session key with out the need of a KDC (Key Distribution Center).
         </p>
         <h2 className={styles.sub_title}>Diffie-Hellman Protocol</h2>
         <p className={styles.the_desc}>
             Alice and Bob chose two numbers p and g which are public.
             <br></br>
-            ‘p’ is a large prime of the order of 1024 bits. ‘g’ is a generator of order p-1 in the group Z p*
+            ‘p’ is a large prime of the order of 1024 bits. ‘g’ is a generator of order p-1 in the group [Z p*, X]
             <br></br>
-            Alice chooses a large random number ‘x’ in the range 0 to p-1 and calculates R1 = g^x mod p
+            Alice chooses a large random number ‘a’ in the range 0 to p-1 and calculates R1 = g^a mod p
             <br></br>
-            Bob chooses a large random number ‘y’ in the range 0 to p-1 and calculates R2 = g^y mod p
+            Bob chooses a large random number ‘b’ in the range 0 to p-1 and calculates R2 = g^b mod p
             <br></br>
             Alice sends R1 to Bob and Bob sends R2 to Alice
             <br></br>
-            Alice Calculates K = R2^x mod p
+            Alice Calculates Ka = (R2)^a mod p
             <br></br>
-            Bob Calculates K = R1^y mod p
+            Bob Calculates Kb = (R1)^b mod p
             <br></br>
-            K = (g^x mod p)^y mod p = (g^y mod p)^x
- mod p = g^xy mod p
+            K = (g ^a mod p)b mod p = (g(^b mod p)a
+ mod p = g^ab mod p
             <br></br>
             K is the symmetric key for the session
             <br></br>
