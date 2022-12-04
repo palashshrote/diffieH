82856c74680bc8a736f059fd4246932fc39db7bc
palashshrote <palash.shrote.58@gmail.com>
2022-12-04 18:38:31 +0530

    hope its final

--- components/Footer.js
+++ components/Footer.js
@@ -4,14 +4,17 @@ import styles from '../styles/Home.module.css'
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <h2>Session 2022-2023</h2><br></br>
-      <h3>Developed by:  </h3><br></br>
-      <div className={styles.abt_names}>
-        Mansi Keshattiwar(A-19)<br></br>
-        Nikita Sarade(A-22)<br></br>
-        Palash Shrote(A-68)<br></br>
-      </div>
       
+      <div className={styles.centr}>
+        <h2>Session 2022-2023</h2>
+      
+      <h3>Developed by:  </h3>
+      <div className={styles.abt_names}>
+        Mansi Keshattiwar  (A-19)<br></br>
+        Nikita Sarade      (A-22)<br></br>
+        Palash Shrote      (A-68)<br></br>
+      </div>
+      </div>
       
     </footer>
   )
