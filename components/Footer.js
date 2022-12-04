2dbe25c9efaa28bcb18b3579703513b929485127
mansikeshattiwar <85787927+mansikeshattiwar@users.noreply.github.com>
2022-12-04 18:16:51 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ import styles from '../styles/Home.module.css'
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <h2>Session 2022-2023</h2>
-      <h3>Developed by:  </h3><br>
+      <h2>Session 2022-2023</h2><br></br>
+      <h3>Developed by:  </h3><br></br>
       <div className={styles.abt_names}>
         Mansi Keshattiwar(A-19)<br></br>
         Nikita Sarade(A-22)<br></br>
